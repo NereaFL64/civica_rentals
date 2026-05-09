7766fc366259c2b9b6755315f0aff03453f4e6c0
Nerea Fernández <nereaferluq@gmail.com>
2026-05-09 12:42:25 +0000

    Arreglos en la relación entre ubicacion y usuario que no funcionaba bien

--- models/silver/intermediate/int_ubicacion.sql
+++ models/silver/intermediate/int_ubicacion.sql
@@ -4,7 +4,9 @@ WITH ubicaciones AS (
           COALESCE(NULLIF(TRIM(direccion), ''), 'Dirección desconocida') AS calle
         , UPPER(TRIM(ciudad)) AS nombre_ciudad
         , UPPER(TRIM(pais)) AS nombre_pais
+
     FROM {{ ref('stg_usuarios') }}
+
     WHERE ciudad IS NOT NULL
       AND pais IS NOT NULL
 
@@ -14,8 +16,9 @@ ciudades AS (
 
     SELECT
           c.id_ciudad
-        , c.nombre AS nombre_ciudad
-        , p.nombre AS nombre_pais
+        , UPPER(TRIM(c.nombre)) AS nombre_ciudad
+        , UPPER(TRIM(p.nombre)) AS nombre_pais
+
     FROM {{ ref('int_ciudad') }} c
 
     LEFT JOIN {{ ref('int_pais') }} p
@@ -23,32 +26,24 @@ ciudades AS (
 
 ),
 
-joined AS (
+final AS (
 
     SELECT
-          {{ dbt_utils.generate_surrogate_key(['u.calle', 'u.nombre_ciudad', 'u.nombre_pais']) }} AS id_ubicacion
+          ROW_NUMBER() OVER (
+              ORDER BY u.nombre_pais, u.nombre_ciudad, u.calle
+          ) AS id_ubicacion
+
         , u.calle
         , c.id_ciudad
 
     FROM ubicaciones u
 
-    INNER JOIN ciudades c
+    LEFT JOIN ciudades c
         ON u.nombre_ciudad = c.nombre_ciudad
        AND u.nombre_pais = c.nombre_pais
-
-),
-
-final AS (
-
-    SELECT *
-    FROM joined
-
-    QUALIFY ROW_NUMBER() OVER (
-        PARTITION BY id_ubicacion
-        ORDER BY calle
-    ) = 1
 
 )
 
 SELECT *
 FROM final
+WHERE id_ciudad IS NOT NULL
